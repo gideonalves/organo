3bf0f5aed3a389b187b0d0a2324a3398a348f823
gideonalves <gideonam@yahoo.com.br>
2023-08-20 16:57:55 -0300

    created collaborative component

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+      <div className='colaborador'>
+         <div className='cabecalho'>
+            <img src='https://avatars.githubusercontent.com/u/66545644?v=4' alt='Gideon' />
+        </div>
+        <div className='rodape'>
+            <h4>Gideon</h4>
+            <h5>Developer Web</h5>
+        </div>
+      </div>
+    )
+}
+
+export default Colaborador
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,15 +6,6 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
-
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
@@ -56,7 +47,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                     obrigatorio={true} 
                     label="Time" 
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
